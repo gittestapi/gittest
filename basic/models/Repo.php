26fd1531e2f662b8f5fcf46abf1c461e24699d73
Liu Di <turbulence@live.cn>
2017-09-21 15:48:38 +0800

    表joinrepo添加字段role,并实现repo和 joinrepo的联动

--- basic/models/Repo.php
+++ basic/models/Repo.php
@@ -60,4 +60,14 @@ class Repo extends \yii\db\ActiveRecord
 			return false;
 		}
 	}
+
+    public function afterDelete()
+    {
+        // ...custom code here...
+        Yii::$app->db->createCommand("delete from joinrepo where repoid=:repoid")
+                ->bindValue(':repoid',$this->repoid)
+                ->execute();
+                   
+        parent::afterDelete();   
+    }
 }
