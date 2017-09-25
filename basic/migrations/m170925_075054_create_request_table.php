a6352567820961b99aa51e8f7a0614e985ea1d13
Liu Di <turbulence@live.cn>
2017-09-25 16:27:13 +0800

    创建表request (功能：申请或邀请用户加入Repo)和表 messagetemplate(功能：message模板)

---
+++ basic/migrations/m170925_075054_create_request_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `request`.
+ */
+class m170925_075054_create_request_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('request', [
+            'id' => $this->primaryKey(),
+            'applicantID' => $this->integer(),
+            'approverID' => $this->integer(),
+            'repoID' => $this->integer(),
+            'mtID' => $this->integer(),
+            'isApproved' => $this->char(1),
+            'created_at' => $this->datetime()
+        ],"ENGINE=InnoDB DEFAULT CHARSET=utf8");
+
+        $this->addForeignKey("ref_user1",'request','applicantID','user','uid',"SET NULL");
+        $this->addForeignKey("ref_user2",'request','approverID','user','uid',"SET NULL");
+        $this->addForeignKey("f_repo","request","repoID","repo","repoid","SET NULL");
+        $this->addForeignKey("ref_messagetemplate","request","mtID","messagetemplate","id","SET NULL");
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('request');
+    }
+}
