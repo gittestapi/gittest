980b1a57ee5113c8590e04530cdcfbe84548632c
Liu Di <turbulence@live.cn>
2017-09-28 19:58:09 +0800

    删除了一个权限和一个rule,删除表 messagetemplate

--- basic/commands/RbacController.php
+++ basic/commands/RbacController.php
@@ -13,8 +13,6 @@ class RbacController extends Controller
 		// add the rules
 		$rule1 = new \app\rbac\RepoAuthorRule;
 		$auth->add($rule1);
-		$rule2 = new \app\rbac\RepoViewRule;
-		$auth->add($rule2);
 
 		//add "changeRepo" permission
 		$changeRepo = $auth->createPermission('changeRepo');
@@ -22,17 +20,10 @@ class RbacController extends Controller
 		$changeRepo->ruleName = $rule1->name;
 		$auth->add($changeRepo);
 
-		// add "viewRepo" permission
-		$viewRepo = $auth->createPermission('viewRepo');
-		$viewRepo->description = 'view Repo entity';
-		$viewRepo->ruleName = $rule2->name;
-		$auth->add($viewRepo);
-
 		// add "commonUser" role
 		$commonUser = $auth->createRole("commonUser");
 		$auth->add($commonUser);
 		$auth->addChild($commonUser,$changeRepo);
-		$auth->addChild($commonUser,$viewRepo);
 
 		// 将默认的账号user001添加到角色commonUser中
 		$auth->assign($commonUser,1);
