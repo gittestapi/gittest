dbdee27c68d3b8bf6954e693e56ce6998c01e5b7
yangzhenping <yzpcreate@hotmail.com>
2017-09-30 00:21:47 -0700

    joinrepo to JoinRepo, and ref_repo to ref_Repo

--- basic/migrations/m170921_093425_add_foreignKey_to_joinrepo_table.php
+++ basic/migrations/m170921_093425_add_foreignKey_to_joinrepo_table.php
@@ -6,12 +6,12 @@ class m170921_093425_add_foreignKey_to_joinrepo_table extends Migration
 {
     public function safeUp()
     {
-        $this->addForeignKey("ref_repo","joinrepo","repoid","repo","repoid","CASCADE","CASCADE");
+        $this->addForeignKey("ref_Repo","JoinRepo","repoid","repo","repoid","CASCADE","CASCADE");
     }
 
     public function safeDown()
     {
-        $this->dropForeignKey("ref_repo","joinrepo");
+        $this->dropForeignKey("ref_Repo","JoinRepo");
     }
 
     /*
