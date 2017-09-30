2678ce5dc997ad7eed8ee6b2cc068857e8852d06
yangzhenping <yzpcreate@hotmail.com>
2017-09-30 02:45:08 -0700

    String(role)

--- basic/web/js/requests.js
+++ basic/web/js/requests.js
@@ -50,7 +50,7 @@ $(function(){
 					callback: function (result) {
 						role = result;
 						for(var i = 0; i< roles.length; i++) {
-							if(roles[i].toUpperCase() == role.trim().toUpperCase()) {
+							if(roles[i].toUpperCase() == String(role).trim().toUpperCase()) {
 								url = url + '&role=' + role.toUpperCase(); // 将 role 的值添加到 url 中
 							}
 						}
