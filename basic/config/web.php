8f6f8f2db78c8ccd41e775a1189602c21001e7fa
Jumping Yang[Wicresoft] <v-zhenpy@microsoft.com>
2017-10-03 11:22:52 +0800

    disable gii

--- basic/config/web.php
+++ basic/config/web.php
@@ -62,12 +62,13 @@ if (YII_ENV_DEV) {
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 
-    $config['bootstrap'][] = 'gii';
+    /*$config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         'allowedIPs' => ['127.0.0.1', '::1','10.249.157.133'],
     ];
+    */
 }
 
 return $config;
