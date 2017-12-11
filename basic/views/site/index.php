1132dfa9096dfaf37538edaea7abd52bfc26ec3d
Liu Di <turbulence@live.cn>
2017-12-11 15:29:52 +0800

    首页添加 “我的测试任务” 链接

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -19,7 +19,7 @@ $this->title = 'GitTest.com -- Manage your tests in GitHub';
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
+            <div class="col-lg-3">
                 <h2>Create Project/Repo</h2>
 
                 <p>Now you can create project/repo in GitTest to manage your team's test, here we go!</p>
@@ -28,7 +28,7 @@ $this->title = 'GitTest.com -- Manage your tests in GitHub';
 		<p><a class="btn btn-default" href="/index.php?r=repo%2Findex">My Project/Repo &raquo;</a></p>
 		<p><a class="btn btn-default" href="/index.php?r=repo%2Findexall">All Project/Repo &raquo;</a></p>
             </div>
-            <div class="col-lg-4">
+            <div class="col-lg-3">
                 <h2>Create Test Case</h2>
 
                 <p>Now you can create test cases in GitTest to manage your team's test, here we go!</p>
@@ -36,7 +36,7 @@ $this->title = 'GitTest.com -- Manage your tests in GitHub';
                 <p><a class="btn btn-default" href="/index.php?r=test-case%2Fcreate">New Test Cases &raquo;</a></p>
 		<p><a class="btn btn-default" href="/index.php?r=test-case%2Findex">My Test Cases &raquo;</a></p>
             </div>
-	    <div class="col-lg-4">
+	    <div class="col-lg-3">
                 <h2>Create Test Plan</h2>
 
                 <p>Now you can create test plan in GitTest to manage your team's test, here we go!</p>
@@ -44,6 +44,12 @@ $this->title = 'GitTest.com -- Manage your tests in GitHub';
                 <p><a class="btn btn-default" href="/index.php?r=test-excution%2Fcreate">New Test Plan &raquo;</a></p>
 		<p><a class="btn btn-default" href="/index.php?r=test-excution%2Findex">My Test Plan &raquo;</a></p>
             </div>
+            <div class="col-lg-3">
+                    <h2>我的测试任务</h2>
+
+                    <p>我作为Tester应该执行的测试任务</p>
+    		<p><a class="btn btn-default" href="/index.php?r=test-excution%2Fmy-missions">我的测试任务 &raquo;</a></p>
+                </div>
         </div>
 
     </div>
